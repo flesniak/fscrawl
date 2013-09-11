a85e3526c705e939cb0cf3bc89ea90ee7f7b0103
Fabian Lesniak <fabian.lesniak@student.kit.edu>
2013-09-11 23:36:39 +0200

    added command line options for tables

--- fscrawl.cpp
+++ fscrawl.cpp
@@ -13,20 +13,22 @@
 using namespace std;
 
 void usage() {
-  cout << "Usage: fscrawl <MODE> [OPTIONS] <basedir|-c>" << endl;
-  cout << "  -h, --help\tDisplay this help and exit" << endl;
-  cout << "  -u, --user\tSpecify database user (default: \"root\")" << endl;
-  cout << "  -p, --password\tSpecify database user password (default: none)" << endl;
-  cout << "  -m, --host\tConnect to host (default: \"localhost\"" << endl;
-  cout << "  -d, --database\tDatabase to use (default: \"fscrawl\")" << endl;
-  cout << "  -f, --fakepath\tInstead of having basedir as absolute root directory, parse all files as if they were unter this fakepath" << endl;
-  cout << "  -v, --verbose\tIncrease log level (0x=info,1x=detailed,2x=debug)" << endl;
-  cout << "  -q, --quiet\tOnly display severe errors. Useful for cron-jobbing" << endl;
-  cout << endl << "The following options may be called without a basedir, then no scanning will be done:" << endl;
-  cout << "  -c, --clear\tDelete the tree for this fakepath, others will be kept" << endl;
-  cout << "  -C, --clearall\tClear the database - ALL DATA WILL BE DELETED" << endl;
-  cout << "  -V, --verify\tVerify the tree structure - takes some time" << endl;
-  cout << endl << "fscrawl version "VERSION << endl;
+  cout << "Usage: fscrawl <MODE> [OPTIONS] <basedir|-c>";
+  cout << "  -h, --help\tDisplay this help and exit";
+  cout << "  -u, --user\tSpecify database user (default: \"root\")";
+  cout << "  -p, --password\tSpecify database user password (default: none)";
+  cout << "  -m, --host\tConnect to host (default: \"localhost\"";
+  cout << "  -d, --database\tDatabase to use (default: \"fscrawl\")";
+  cout << "  -f, --fakepath\tInstead of having basedir as absolute root directory, parse all files as if they were unter this fakepath";
+  cout << "  --file-table\tTable to use for files (default: \"fscrawl_files\")";
+  cout << "  --dir-table\tTable to use for directories (default: \"fscrawl_directories\")";
+  cout << "  -v, --verbose\tIncrease log level (0x=info,1x=detailed,2x=debug)";
+  cout << "  -q, --quiet\tOnly display severe errors. Useful for cron-jobbing";
+  cout << "The following options may be called without a basedir, then no scanning will be done:";
+  cout << "  -c, --clear\tDelete the tree for this fakepath, others will be kept";
+  cout << "  -C, --clearall\tClear the database - ALL DATA WILL BE DELETED";
+  cout << "  -V, --verify\tVerify the tree structure - takes some time";
+  cout << "fscrawl version "VERSION;
 }
 
 int main(int argc, char* argv[]) {
@@ -34,6 +36,8 @@ int main(int argc, char* argv[]) {
   unsigned char clear = 0;
   string basedir;
   string fakepath;
+  string directoryTable;
+  string fileTable;
   string mysql_user("root");
   string mysql_password;
   string mysql_host("localhost");
@@ -45,7 +49,7 @@ int main(int argc, char* argv[]) {
       usage();
       exit(1);
     }
-    if( strcmp(argv[i],"-v") == 0 || strcmp(argv[i],"--verbose") == 0) {
+    if( strcmp(argv[i],"-v") == 0 || strcmp(argv[i],"--verbose") == 0 ) {
       Logger::logLevel() = (logLevel_t)( (uint32_t)Logger::logLevel() + 1 );
       continue;
     }
@@ -61,9 +65,9 @@ int main(int argc, char* argv[]) {
       Logger::logLevel() = logError;
       continue;
     }
-    if( strcmp(argv[i],"-u") == 0 || strcmp(argv[i],"--user") == 0) {
+    if( strcmp(argv[i],"-u") == 0 || strcmp(argv[i],"--user") == 0 ) {
       if( i+1 >= argc ) {
-        cerr << "ERROR: expected user" << endl;
+        LOG(logError) << "ERROR: expected user";
         usage();
         exit(1);
       }
@@ -71,9 +75,9 @@ int main(int argc, char* argv[]) {
       mysql_user = argv[i];
       continue;
     }
-    if( strcmp(argv[i],"-p") == 0 || strcmp(argv[i],"--password") == 0) {
+    if( strcmp(argv[i],"-p") == 0 || strcmp(argv[i],"--password") == 0 ) {
       if( i+1 >= argc ) {
-        cerr << "ERROR: expected password" << endl;
+        LOG(logError) << "ERROR: expected password";
         usage();
         exit(1);
       }
@@ -81,9 +85,9 @@ int main(int argc, char* argv[]) {
       mysql_password = argv[i];
       continue;
     }
-    if( strcmp(argv[i],"-m") == 0 || strcmp(argv[i],"--host") == 0) {
+    if( strcmp(argv[i],"-m") == 0 || strcmp(argv[i],"--host") == 0 ) {
       if( i+1 >= argc ) {
-        cerr << "ERROR: expected host" << endl;
+        LOG(logError) << "ERROR: expected host";
         usage();
         exit(1);
       }
@@ -91,9 +95,9 @@ int main(int argc, char* argv[]) {
       mysql_host = argv[i];
       continue;
     }
-    if( strcmp(argv[i],"-d") == 0 || strcmp(argv[i],"--database") == 0) {
+    if( strcmp(argv[i],"-d") == 0 || strcmp(argv[i],"--database") == 0 ) {
       if( i+1 >= argc ) {
-        cerr << "ERROR: expected database" << endl;
+        LOG(logError) << "ERROR: expected database";
         usage();
         exit(1);
       }
@@ -101,13 +105,33 @@ int main(int argc, char* argv[]) {
       mysql_database = argv[i];
       continue;
     }
-    if( strcmp(argv[i],"-C") == 0 || strcmp(argv[i],"--clearall") == 0) {
+    if( strcmp(argv[i],"--file-table") == 0 ) {
+      if( i+1 >= argc ) {
+        LOG(logError) << "ERROR: expected file table";
+        usage();
+        exit(1);
+      }
+      i++;
+      fileTable = argv[i];
+      continue;
+    }
+    if( strcmp(argv[i],"--dir-table") == 0 ) {
+      if( i+1 >= argc ) {
+        LOG(logError) << "ERROR: expected directory table";
+        usage();
+        exit(1);
+      }
+      i++;
+      directoryTable = argv[i];
+      continue;
+    }
+    if( strcmp(argv[i],"-C") == 0 || strcmp(argv[i],"--clearall") == 0 ) {
       clear = true;
       continue;
     }
-    if( strcmp(argv[i],"-f") == 0 || strcmp(argv[i],"--fakepath") == 0) {
+    if( strcmp(argv[i],"-f") == 0 || strcmp(argv[i],"--fakepath") == 0 ) {
       if( i+1 >= argc ) {
-        cerr << "ERROR: expected fakepath" << endl;
+        LOG(logError) << "ERROR: expected fakepath";
         usage();
         exit(1);
       }
@@ -115,19 +139,19 @@ int main(int argc, char* argv[]) {
       fakepath = argv[i];
       continue;
     }
-    if( strcmp(argv[i],"-V") == 0 || strcmp(argv[i],"--verify") == 0) {
+    if( strcmp(argv[i],"-V") == 0 || strcmp(argv[i],"--verify") == 0 ) {
       verify = true;
       continue;
     }
     if( strncmp(argv[i],"-",1) == 0 ) {
-      cerr << "ERROR: unknown argument " << argv[i] << endl;
+      LOG(logError) << "ERROR: unknown argument " << argv[i];
       usage();
       exit(1);
     }
     if( basedir.empty() )
       basedir = argv[i];
     else {
-      cerr << "ERROR: invalid additional argument " << argv[i] << endl;
+      LOG(logError) << "ERROR: invalid additional argument " << argv[i];
       usage();
       exit(1);
     }
@@ -135,7 +159,7 @@ int main(int argc, char* argv[]) {
 
   if( basedir.empty() ) {
     if( !clear && !verify ) { //if no basedir is given but clear is set, only clear the db and exit
-      cout << "ERROR: no basedir given" << endl;
+      cout << "ERROR: no basedir given";
       usage();
       exit(1);
     }
@@ -152,6 +176,7 @@ int main(int argc, char* argv[]) {
   con->setSchema(mysql_database);
 
   worker* w = new worker(con);
+  w->setTables(directoryTable,fileTable);
 
   if( clear == 2 ) {
     w->clearDatabase();
@@ -162,7 +187,7 @@ int main(int argc, char* argv[]) {
   if( !basedir.empty() ) {
     //ascend to given fakepath
     uint32_t fakepathId = w->ascendPath(fakepath);
-    cout << "fakepathId " << fakepathId << endl;
+    cout << "fakepathId " << fakepathId;
     /*//Save starting time
     LOG(logDebug) << "starting parser";
     time_t start = time(0);
