1591368d13c3f18eaa465ea8f30ee63453d8c2ad
Fabian Lesniak <fabian.lesniak@student.kit.edu>
2013-09-21 00:43:35 +0200

    fixed command line logic
    - log output updated
    - fixed bug not printing usage when no basedir is given

--- fscrawl.cpp
+++ fscrawl.cpp
@@ -13,29 +13,29 @@
 using namespace std;
 
 void usage() {
-  cout << "Usage: fscrawl <MODE> [OPTIONS] <basedir|-c>";
-  cout << "  -h, --help\tDisplay this help and exit";
-  cout << "  -u, --user\tSpecify database user (default: \"root\")";
-  cout << "  -p, --password\tSpecify database user password (default: none)";
-  cout << "  -m, --host\tConnect to host (default: \"localhost\"";
-  cout << "  -d, --database\tDatabase to use (default: \"fscrawl\")";
-  cout << "  -f, --fakepath\tInstead of having basedir as absolute root directory, parse all files as if they were unter this fakepath";
-  cout << "  --file-table\tTable to use for files (default: \"fscrawl_files\")";
-  cout << "  --dir-table\tTable to use for directories (default: \"fscrawl_directories\")";
-  cout << "  -v, --verbose\tIncrease log level (0x=info,1x=detailed,2x=debug)";
-  cout << "  -q, --quiet\tOnly display severe errors. Useful for cron-jobbing";
-  cout << "  -w, --watch\tWatch the given basedir after refreshing (program will block)";
-  cout << "The following options may be called without a basedir, then no scanning will be done:";
-  cout << "  -c, --clear\tDelete the tree for this fakepath, others will be kept";
-  cout << "  -C, --clearall\tClear both tables completely";
-  cout << "  -V, --verify\tVerify the tree structure - takes some time";
-  cout << "fscrawl version "VERSION;
+  LOG(logInfo) << "Usage: fscrawl <MODE> [OPTIONS] <basedir|-c>";
+  LOG(logInfo) << "  -h, --help\tDisplay this help and exit";
+  LOG(logInfo) << "  -u, --user\tSpecify database user (default: \"root\")";
+  LOG(logInfo) << "  -p, --password\tSpecify database user password (default: none)";
+  LOG(logInfo) << "  -m, --host\tConnect to host (default: \"localhost\"";
+  LOG(logInfo) << "  -d, --database\tDatabase to use (default: \"fscrawl\")";
+  LOG(logInfo) << "  -f, --fakepath\tInstead of having basedir as absolute root directory, parse all files as if they were unter this fakepath";
+  LOG(logInfo) << "  --file-table\tTable to use for files (default: \"fscrawl_files\")";
+  LOG(logInfo) << "  --dir-table\tTable to use for directories (default: \"fscrawl_directories\")";
+  LOG(logInfo) << "  -v, --verbose\tIncrease log level (0x=info,1x=detailed,2x=debug)";
+  LOG(logInfo) << "  -q, --quiet\tOnly display severe errors. Useful for cron-jobbing";
+  LOG(logInfo) << "  -w, --watch\tWatch the given basedir after refreshing (program will block)";
+  LOG(logInfo) << "The following options may be called without a basedir, then no scanning will be done:";
+  LOG(logInfo) << "  -c, --clear\tDelete the tree for this fakepath, others will be kept";
+  LOG(logInfo) << "  -C, --clearall\tClear both tables completely";
+  LOG(logInfo) << "  -V, --verify\tVerify the tree structure - takes some time";
+  LOG(logInfo) << "fscrawl version "VERSION;
 }
 
 int main(int argc, char* argv[]) {
   bool verify = false;
   bool watch = false;
-  enum { clearNothing, clearFakepath, clearAll } clear;
+  enum { clearNothing, clearFakepath, clearAll } clear = clearNothing;
   string basedir;
   string fakepath;
   string directoryTable;
@@ -45,6 +45,7 @@ int main(int argc, char* argv[]) {
   string mysql_host("localhost");
   string mysql_database("fscrawl");
   Logger::logLevel() = logInfo;
+  Logger::facility() = new LoggerFacilityConsole;
 
   for(int i = 1; i < argc; i++) {
     if( strcmp(argv[i],"-h") == 0 || strcmp(argv[i],"--help") == 0 ) {
@@ -69,7 +70,7 @@ int main(int argc, char* argv[]) {
     }
     if( strcmp(argv[i],"-u") == 0 || strcmp(argv[i],"--user") == 0 ) {
       if( i+1 >= argc ) {
-        LOG(logError) << "ERROR: expected user";
+        LOG(logError) << "expected user";
         usage();
         exit(1);
       }
@@ -79,7 +80,7 @@ int main(int argc, char* argv[]) {
     }
     if( strcmp(argv[i],"-p") == 0 || strcmp(argv[i],"--password") == 0 ) {
       if( i+1 >= argc ) {
-        LOG(logError) << "ERROR: expected password";
+        LOG(logError) << "expected password";
         usage();
         exit(1);
       }
@@ -89,7 +90,7 @@ int main(int argc, char* argv[]) {
     }
     if( strcmp(argv[i],"-m") == 0 || strcmp(argv[i],"--host") == 0 ) {
       if( i+1 >= argc ) {
-        LOG(logError) << "ERROR: expected host";
+        LOG(logError) << "expected host";
         usage();
         exit(1);
       }
@@ -99,7 +100,7 @@ int main(int argc, char* argv[]) {
     }
     if( strcmp(argv[i],"-d") == 0 || strcmp(argv[i],"--database") == 0 ) {
       if( i+1 >= argc ) {
-        LOG(logError) << "ERROR: expected database";
+        LOG(logError) << "expected database";
         usage();
         exit(1);
       }
@@ -109,7 +110,7 @@ int main(int argc, char* argv[]) {
     }
     if( strcmp(argv[i],"--file-table") == 0 ) {
       if( i+1 >= argc ) {
-        LOG(logError) << "ERROR: expected file table";
+        LOG(logError) << "expected file table";
         usage();
         exit(1);
       }
@@ -119,7 +120,7 @@ int main(int argc, char* argv[]) {
     }
     if( strcmp(argv[i],"--dir-table") == 0 ) {
       if( i+1 >= argc ) {
-        LOG(logError) << "ERROR: expected directory table";
+        LOG(logError) << "expected directory table";
         usage();
         exit(1);
       }
@@ -137,7 +138,7 @@ int main(int argc, char* argv[]) {
     }
     if( strcmp(argv[i],"-f") == 0 || strcmp(argv[i],"--fakepath") == 0 ) {
       if( i+1 >= argc ) {
-        LOG(logError) << "ERROR: expected fakepath";
+        LOG(logError) << "expected fakepath";
         usage();
         exit(1);
       }
@@ -154,22 +155,22 @@ int main(int argc, char* argv[]) {
       continue;
     }
     if( strncmp(argv[i],"-",1) == 0 ) {
-      LOG(logError) << "ERROR: unknown argument " << argv[i];
+      LOG(logError) << "unknown argument " << argv[i];
       usage();
       exit(1);
     }
     if( basedir.empty() )
       basedir = argv[i];
     else {
-      LOG(logError) << "ERROR: invalid additional argument " << argv[i];
+      LOG(logError) << "invalid additional argument " << argv[i];
       usage();
       exit(1);
     }
   }
 
   if( basedir.empty() ) {
-    if( !clear && !verify ) { //if no basedir is given but clear is set, only clear the db and exit
-      LOG(logError) << "ERROR: no basedir given";
+    if( clear == clearNothing && !verify ) { //if no basedir is given but clear is set, only clear the db and exit
+      LOG(logError) << "no basedir given";
       usage();
       exit(1);
     }
@@ -177,8 +178,6 @@ int main(int argc, char* argv[]) {
   else
     if( basedir.at(basedir.size()-1) == '/' ) //don't try that on an empty basedir string
       basedir.erase(basedir.size()-1);
-
-  Logger::facility() = new LoggerFacilityConsole;
 
   LOG(logInfo) << "Connecting to SQL server";
   sql::Driver *driver = get_driver_instance();
